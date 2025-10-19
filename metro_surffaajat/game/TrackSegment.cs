87c428889ec56923ec1c2493f93219935f3d6238
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2025-10-19 20:19:46 +0300

    Implemented ObstacleSegment class for storing a row of obstacles

--- metro_surffaajat/game/TrackSegment.cs
+++ metro_surffaajat/game/TrackSegment.cs
@@ -5,6 +5,92 @@ using System.Linq;
 namespace Metro_Surffaajat.game;
 
 /// <summary>
+/// Structure for storing a segment of obstacles.
+/// </summary>
+public class ObstacleSegment
+{
+    /// <summary>
+    /// Maximum count of elements in internal array.
+    /// </summary>
+    private const int Capacity = 3;
+    /// <summary>
+    /// Count of elements in internal array.
+    /// </summary>
+    public readonly int Length = 3;
+    /// <summary>
+    /// Internal array of obstacles. 
+    /// </summary>
+    private readonly ObstacleType[] _obstacles;
+    /// <summary>
+    /// Segment indices for each obstacle.
+    /// </summary>
+    private readonly uint[] _segmentIndices;
+    
+    /// <summary>
+    /// Gets obstacle type at specified index.
+    /// </summary>
+    /// <param name="index">Index of the obstacle to get.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Specified index has to be less than Length</exception>
+    public ObstacleType this[uint index] {
+        get
+        {
+            if (index > Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), index, $"was not valid index of ObstacleSegment with Length {Length}");
+            }
+
+            return _obstacles[index];
+        }
+    }
+    
+    
+    /// <summary>
+    /// Create an ObstacleSegment.
+    /// </summary>
+    public ObstacleSegment()
+    {
+        _segmentIndices = [0, 0, 0];
+        _obstacles = [ObstacleType.None, ObstacleType.None, ObstacleType.None];
+    }
+
+    
+    /// <summary>
+    /// Create an ObstacleSegment with specified obstacles and indices.
+    /// </summary>
+    /// <param name="obstacles">Obstacles in the segment. Array length should equal Capacity</param>
+    /// <param name="segmentIndices">Indices of each obstacle in the segment. Array length should equal Capacity</param>
+    private ObstacleSegment(ObstacleType[] obstacles, uint[] segmentIndices)
+    {   
+        Debug.Assert(obstacles.Length == Capacity);
+        Debug.Assert(segmentIndices.Length == Capacity);
+        
+        _segmentIndices = segmentIndices;
+        _obstacles = obstacles;
+    }
+    
+    
+    /// <summary>
+    /// Calculates the following ObstacleSegment for this segment.
+    /// </summary>
+    /// <returns>The next ObstacleSegment</returns>
+    public ObstacleSegment GetNext()
+    {
+        ObstacleType[] nextObstacles = new ObstacleType[Capacity];
+        uint[] nextIndices = new uint[Capacity];
+        
+        for (int i = 0; i < Capacity; i++)
+        {
+            nextObstacles[i] = Obstacles.GetNext(_obstacles[i]);
+            nextIndices[i] = nextObstacles[i] == _obstacles[i] ? _segmentIndices[i] + 1 : 0;
+        }
+
+        return new ObstacleSegment(nextObstacles, nextIndices);
+    }
+}
+
+
+
+/// <summary>
 /// Structure for storing a segment of coins.
 /// </summary>
 public class CoinSegment
